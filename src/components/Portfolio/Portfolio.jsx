45810023cc3490c835eea9016c9be6a4ce8d4939
Kanokwan190946 <65020733@up.ac.th>
2024-03-06 23:55:36 +0700

    first commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import styles from './Portfolio.module.css'
 
 const user ={
-  img1:'https://pin.it/7JYwAyHT2',
-  img2:'https://pin.it/2wD6KqUrJ',
-  img3:'https://pin.it/3eVJpqUok',
-  img4:'https://pin.it/36EZacUES',
-  img5:'https://pin.it/1kwuVqeow',
-  img6:'https://pin.it/34yZg8ueK',
+  img1:'https://i.pinimg.com/564x/ee/3b/1b/ee3b1b75d0e41e022d571d19c4d4a96f.jpg',
+  img2:'https://i.pinimg.com/564x/89/41/65/894165f3830477647d582800e9b5ed0c.jpg',
+  img3:'https://i.pinimg.com/564x/08/f4/ea/08f4eaa4e0625af972599365dfeec6b6.jpg',
+  img4:'https://i.pinimg.com/564x/a2/46/a3/a246a38a46de6e005e7b851009af9f11.jpg',
+  img5:'https://i.pinimg.com/564x/b9/b0/6f/b9b06fdb71c126d069da4b328993df3f.jpg',
+  img6:'https://i.pinimg.com/564x/16/04/93/160493dbe7df1c370cfd035e06595c4c.jpg',
 }
 function Portfolio() {
   return (
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./Testimonials.module.css";
 
 const user ={
-  img7:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.akerufeed.com%2Ftrend-life%2F30-boyfriend-picture-ideas&psig=AOvVaw3gDWHupQsky2T2028M3Jhf&ust=1709829579375000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqGAoTCICUrdiJ4IQDFQAAAAAdAAAAABCeAQ',
-  img8:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ladyissue.com%2F218973%2F&psig=AOvVaw14g4xVrS03yXM7ZcDDBesq&ust=1709829796916000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDJh8CK4IQDFQAAAAAdAAAAABAE',
-  img9:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ladyissue.com%2F218973%2F&psig=AOvVaw14g4xVrS03yXM7ZcDDBesq&ust=1709829796916000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDJh8CK4IQDFQAAAAAdAAAAABAI',
+  img7:'https://www.akerufeed.com/wp-content/uploads/2019/09/2-55.jpg',
+  img8:'https://www.ladyissue.com/wp-content/uploads/2021/11/2-23-2.jpg',
+  img9:'https://www.ladyissue.com/wp-content/uploads/2021/11/2-16-5.jpg',
 }
 function Testimonials() {
   return (
